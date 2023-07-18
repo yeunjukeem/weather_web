da4748b5e4ae64d5786fa88f71d49291df4284a6
Your Name <you@example.com>
2023-07-18 14:48:46 +0900

    weather_web

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
 #app.py_weather_web
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from weather import Weather
 
 app = Flask(__name__)
 
-@app.route('/', methods=['get','post'])
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method =='GET':
+        weather_result = {}
+        error = 0
+        return render_template('index.html', weather = weather_result, error=error)
+    elif request.method == 'POST':
+        city_name = request.form['city_name']
+        if city_name:
+            weather = Weather(city_name=city_name)
+            weather_result = weather.get_weather_info()
+            if weather_result['cod'] == 200:
+                return render_template('index.html', weather = weather_result, city_name=city_name, error=weather.error)
+
+            else:
+                error = 1
+                return render_template('index.html', weather={}, city_name=city_name, error=error)
+        else:
+            return redirect('/')
+
 
 if __name__== "__main__":
     app.run(debug=True, port=3000)  #포트 바꿀때
--- weather.py
+++ weather.py
@@ -12,7 +12,7 @@ class Weather:
         weather_api_key = "3dc06380d440e4afeac98d7f604cbeb4"
         url = f"https://api.openweathermap.org/data/2.5/weather?q={self.city_name},kor&appid={weather_api_key}"
         self.weather_data = requests.get(url).json()
-        print(self.weather_data)
-
-# weather = Weather(city_name='노원구')        
-# weather.get_weather_info()
+        # print(self.weather_data)
+        return self.weather_data
+weather = Weather(city_name='노원구')        
+weather.get_weather_info()
