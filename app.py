1eeb265f6203392562ebee8b310f80304786f9db
Your Name <you@example.com>
2023-07-18 11:03:18 +0900

    weather class

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 #app.py_weather_web
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+@app.route('/', methods=['get','post'])
+def index():
+    return render_template('index.html')
 
 if __name__== "__main__":
     app.run(debug=True, port=3000)  #포트 바꿀때
---
+++ weather.py
@@ -0,0 +1,18 @@
+#weather.py
+
+import requests
+class Weather:
+    def __init__(self, error=0, city_name='',weather_data={}):
+        self.error= error
+        self.city_name= city_name
+        self.weather_data=weather_data
+
+
+    def get_weather_info(self):
+        weather_api_key = "3dc06380d440e4afeac98d7f604cbeb4"
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={self.city_name},kor&appid={weather_api_key}"
+        self.weather_data = requests.get(url).json()
+        print(self.weather_data)
+
+# weather = Weather(city_name='노원구')        
+# weather.get_weather_info()
